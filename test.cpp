36786bb172a8fd8d99116ce03cc754e2731c289d
mustafa389 <mustafaasghar66@gmail.com>
2023-11-12 13:52:49 +0500

    Update test.cpp
    testing on my end too

--- test.cpp
+++ test.cpp
@@ -4,4 +4,6 @@ int main() {
     int a;
     std::cout << "test to see if new file can be created locally and commited to github" << std::endl;
     std::cin >> a;
+
+    std::cout<<"testing to check whether this works on my end or not too"<<std::endl;
 }
