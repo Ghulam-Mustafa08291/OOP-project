b5188a696764d9910f563c6665ff8205726125ea
mustafa389 <mustafaasghar66@gmail.com>
2023-11-13 12:36:45 +0500

    wrote a code for a basic window to appear

--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    int a;
-    std::cout << "test to see if new file can be created locally and commited to github" << std::endl;
-    std::cin >> a;
-
-    std::cout<<"testing to check whether this works on my end or not too"<<std::endl;
-}
---
+++ testwindow.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <SDL.h>
+
+int main(int argc, char* args[]) {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+    // Main loop flag
+    bool quit = false;
+
+    // Event handler
+    SDL_Event e;
+
+    // Main loop
+    while (!quit) {
+        // Handle events on queue
+        while (SDL_PollEvent(&e) != 0) {
+            // User requests quit
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        // Clear the screen (black)
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // Present the renderer
+        SDL_RenderPresent(renderer);
+    }
+
+    // Destroy window
+    SDL_DestroyWindow(window);
+
+    // Quit SDL subsystems
+    SDL_Quit();
+
+    return 0;
+}
