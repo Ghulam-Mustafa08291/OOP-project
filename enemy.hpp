c4efc9865612bbf0e3a7eacd2b83117bd96fe677
i-ahmed25 <ia08308@st.habib.edu.pk>
2023-11-26 23:22:28 +0500

    enemy class created

---
+++ enemy.hpp
@@ -0,0 +1,24 @@
+#ifndef ENEMY_HPP
+#define ENEMY_HPP
+
+
+#include <SDL.h>
+#include <SDL_image.h>
+
+class Enemy{
+    private:
+        int health;
+        int speed;
+        int damage;
+    
+    public:
+        //getters,setters,constructors,destructors
+        void setHealth(int h);
+        void setSpeed(int s);
+        void setDamage(int d);
+        int getHealth();
+        int getSpeed();
+        int getDamage();
+        Enemy();
+        ~Enemy();
+};
