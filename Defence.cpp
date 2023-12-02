9e3fd1c813fca5ecec96397960baa3c51f68f8b3
mustafa389 <mustafaasghar66@gmail.com>
2023-12-02 16:15:08 +0500

    made the defence plant class

---
+++ Defence.cpp
@@ -0,0 +1,9 @@
+#include "Defence.hpp"
+#include <iostream>
+
+int Defence::get_hits_until_attack(){return this->hits_until_attack;}
+void Defence::set_hits_until_attack(int inp_hits){this->hits_until_attack=inp_hits;}
+
+Defence::Defence(){std::cout<<"Defence type plant has been planted!"<<std::endl;}
+Defence::~Defence(){std::cout<<"bye Defence type plant! (sed noises)"<<std::endl;}
+
---
+++ Defence.hpp
@@ -0,0 +1,23 @@
+#ifndef Defence_HPP
+#define Defence_HPP
+#include "plants.hpp"
+
+#include <SDL.h>
+#include <SDL_image.h>
+
+class Defence:public Plants{
+    private:
+        int hits_until_attack;
+
+    public:
+        //getters and setters and other functions
+        int get_hits_until_attack();
+        void set_hits_until_attack(int hits);
+
+        Defence();
+        ~Defence();
+};
+
+
+
+#endif 
