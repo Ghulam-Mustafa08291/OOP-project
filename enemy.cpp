070c148e47b45c8c332f5df0cd45ab4c9d21b2f2
i-ahmed25 <ia08308@st.habib.edu.pk>
2023-12-03 14:38:48 +0500

    made test enemy movement

--- enemy.cpp
+++ enemy.cpp
@@ -25,6 +25,31 @@ int Enemy::getDamage(){
     return this->damage;
 }
 
+void Enemy::trackPlayer(const Player& player) {
+    // Get player and enemy positions
+    int playerX = player.getPosition().x;
+    int playerY = player.getPosition().y;
+    int enemyX = this->position.x;
+    int enemyY = this->position.y;
+
+    // Calculate the direction towards the player
+    float dx = static_cast<float>(playerX - enemyX);
+    float dy = static_cast<float>(playerY - enemyY);
+
+    // Normalize the direction vector
+    float length = std::sqrt(dx * dx + dy * dy);
+    if (length != 0) {
+        dx /= length;
+        dy /= length;
+    }
+
+    // Update enemy position to move towards the player
+    // Adjust the enemy's position based on its speed
+    float speed = this->getSpeed(); // Get the enemy's speed
+    this->position.x += static_cast<int>(dx * speed);
+    this->position.y += static_cast<int>(dy * speed);
+}
+
 Enemy::Enemy()
 :health{100},speed{1},damage{5} //assuming random values for them now
 {}
--- enemy.hpp
+++ enemy.hpp
@@ -1,23 +1,32 @@
+#ifndef ENEMY_HPP
 #define ENEMY_HPP
-
 
 #include <SDL.h>
 #include <SDL_image.h>
+#include "Player.hpp" // Include the Player class header
 
-class Enemy{
-    private:
-        int health;
-        int speed;
-        int damage;
-    
-    public:
-        //getters,setters,constructors,destructors
-        void setHealth(int h);
-        void setSpeed(int s);
-        void setDamage(int d);
-        int getHealth();
-        int getSpeed();
-        int getDamage();
-        Enemy();
-        ~Enemy();
+class Enemy {
+private:
+    int health;
+    int speed;
+    int damage;
+    SDL_Rect position;
+
+public:
+    // Getters and setters
+    void setHealth(int h);
+    void setSpeed(int s);
+    void setDamage(int d);
+    int getHealth();
+    int getSpeed();
+    int getDamage();
+
+    // Method to track the player
+    void trackPlayer(const Player& player);
+
+    // Constructors and destructor
+    Enemy();
+    ~Enemy();
 };
+
+#endif // ENEMY_HPP
